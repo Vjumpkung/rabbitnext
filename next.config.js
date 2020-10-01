f18ef74daf0f695f491c55fbb265bc02de77bd5d
Chanrich Pisitjing <vjumpkung@gmail.com>
2020-10-01 17:41:29 +0700

    static HTML

--- next.config.js
+++ next.config.js
@@ -8,3 +8,17 @@ module.exports = {
     return config;
   },
 };
+module.exports = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/DTG': { page: '/DTG' },
+      '/DST': { page: '/DST' },
+      '/blockscreen': { page: '/blockscreen' },
+      '/embroidery': { page: '/embroidery' },
+    }
+  },
+}
